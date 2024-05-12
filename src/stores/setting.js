4c6c49ae208e69500cdd83ac6e3175b9dc5f293a
Xiaoming <coderlxm@gmial.com>
2024-05-12 19:34:39 +0800

    optimize the func of buttonOrder

--- src/stores/setting.js
+++ src/stores/setting.js
@@ -34,26 +34,38 @@ export const useSettingStore = defineStore('setting', {
       }
     },
     changeBtnOrder() {
-      const [top, medium, bottom] = Object.values(this.btnOrder)
-      if (top == 1 && medium == 3 && bottom == 2) {
-        this.btnOrder = {
-          orderTop: 1,
-          orderMedium: 2,
-          orderBottom: 3
-        }
-      } else if (top == 1 && medium == 2 && bottom == 3) {
-        this.btnOrder = {
-          orderTop: 2,
-          orderMedium: 1,
-          orderBottom: 3
-        }
-      } else {
-        this.btnOrder = {
-          orderTop: 1,
-          orderMedium: 3,
-          orderBottom: 2
-        }
-      }
+      // 使用映射表定义转换规则
+      const orderMapping = {
+        '1,3,2': { orderTop: 1, orderMedium: 2, orderBottom: 3 },
+        '1,2,3': { orderTop: 2, orderMedium: 1, orderBottom: 3 },
+        'default': { orderTop: 1, orderMedium: 3, orderBottom: 2 }
+      };
+
+      // 从 btnOrder 对象获取当前排序，并生成一个 key 字符串
+      const currentOrderKey = `${this.btnOrder.orderTop},${this.btnOrder.orderMedium},${this.btnOrder.orderBottom}`;
+
+      // 使用映射表或默认值来更新 btnOrder
+      this.btnOrder = orderMapping[currentOrderKey] || orderMapping['default'];
+      // const [top, medium, bottom] = Object.values(this.btnOrder)
+      // if (top == 1 && medium == 3 && bottom == 2) {
+      //   this.btnOrder = {
+      //     orderTop: 1,
+      //     orderMedium: 2,
+      //     orderBottom: 3
+      //   }
+      // } else if (top == 1 && medium == 2 && bottom == 3) {
+      //   this.btnOrder = {
+      //     orderTop: 2,
+      //     orderMedium: 1,
+      //     orderBottom: 3
+      //   }
+      // } else {
+      //   this.btnOrder = {
+      //     orderTop: 1,
+      //     orderMedium: 3,
+      //     orderBottom: 2
+      //   }
+      // }
     }
   },
   persist: true
