2796b7985c7f3db22bd2d0d9327c0d778ebf329f
Xiaoming <coderlxm@gmial.com>
2024-05-12 20:02:29 +0800

    conceive about pages 1/3

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,5 +2,23 @@
 
 </script>
 <template>
-  <div></div>
+  <div class="flex flex-col p-2 mt-10 gap-8 color-#f01654">
+    <div>
+      <p class="font-700 pb-2" style="border-bottom: 1px solid #f01654;">为什么写YoYoScore</p>
+      <p>我不是松浦豪，玩不好球，但至少还能写点代码吧~</p>
+    </div>
+    <div class="flex flex-col gap-2">
+      <p class="pb-2 font-700" style="border-bottom: 1px solid #f01654;">想说的话</p>
+      <div>I don't want to win by making compromises to myself, I've always wanted to win on my own terms.</div>
+      <div style="align-self: flex-end;">--- Kerian Cooper</div>
+    </div>
+    <div class="flex flex-col">
+      <p class="pb-2 font-700" style="border-bottom: 1px solid #f01654;">感谢</p>
+      <ul class="flex flex-col gap-2">
+        <li>Vue.js & Ecosystem</li>
+        <li>IYYF</li>
+        <li>YoClicker</li>
+      </ul>
+    </div>
+  </div>
 </template>
