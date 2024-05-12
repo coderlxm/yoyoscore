45d0b041953b5d433309a49be6df860d010d86c7
Xiaoming <coderlxm@gmial.com>
2024-05-12 20:06:48 +0800

    about page adjust

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,19 +2,19 @@
 
 </script>
 <template>
-  <div class="flex flex-col p-2 mt-10 gap-8 color-#f01654">
-    <div>
-      <p class="font-700 pb-2" style="border-bottom: 1px solid #f01654;">为什么写YoYoScore</p>
-      <p>我不是松浦豪，玩不好球，但至少还能写点代码吧~</p>
+  <div class="flex flex-col p-2 mt-10 gap-10 color-#f01654">
+    <div class="flex flex-col gap-5">
+      <div class="font-700 pb-2" style="border-bottom: 1px solid #f01654;">为什么写YoYoScore</div>
+      <div>我不是松浦豪，玩不好球，但至少还能写点代码吧~</div>
     </div>
-    <div class="flex flex-col gap-2">
-      <p class="pb-2 font-700" style="border-bottom: 1px solid #f01654;">想说的话</p>
+    <div class="flex flex-col gap-5">
+      <div class="pb-2 font-700" style="border-bottom: 1px solid #f01654;">想说的话</div>
       <div>I don't want to win by making compromises to myself, I've always wanted to win on my own terms.</div>
       <div style="align-self: flex-end;">--- Kerian Cooper</div>
     </div>
-    <div class="flex flex-col">
-      <p class="pb-2 font-700" style="border-bottom: 1px solid #f01654;">感谢</p>
-      <ul class="flex flex-col gap-2">
+    <div class="flex flex-col gap-5">
+      <div class="pb-2 font-700" style="border-bottom: 1px solid #f01654;">感谢</div>
+      <ul class="flex flex-col gap-3">
         <li>Vue.js & Ecosystem</li>
         <li>IYYF</li>
         <li>YoClicker</li>
