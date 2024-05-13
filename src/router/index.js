26ce5453f985fa5230bf8d52fd3dc73bd2647f44
Xiaoming <coderlxm@gmial.com>
2024-05-13 16:07:06 +0800

    routes detached

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import routes from './routes'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/result',
-      name: 'result',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ResultView.vue')
-    },
-    {
-      path: '/setting',
-      name: 'setting',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SettingView.vue')
-    },
-    {
-      path: '/record',
-      name: 'record',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RecordView.vue')
-    }
-  ]
+  routes
 })
 
 export default router
