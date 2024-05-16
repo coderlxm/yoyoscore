0be5f7d849a82e623b4ca9a2b599f473d2bf4273
Xiaoming <coderlxm@gmial.com>
2024-05-16 21:37:48 +0800

    delete index store

--- src/stores/index.js
+++
@@ -1,9 +0,0 @@
-import { useRecordStore } from "./record";
-import { useSettingStore } from "./setting";
-import { useResultStore } from "./result";
-import { useScoreStore } from "./score";
-
-export const recordStore = useRecordStore()
-export const settingStore = useSettingStore()
-export const resultStore = useResultStore()
-export const scoreStore = useScoreStore()
